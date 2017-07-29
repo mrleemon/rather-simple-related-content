adcead8cca73bb6207cf855631eb2803ec70f892
leemon <tacrapo@gmail.com>
2017-07-29 21:20:07 +0200

    First commit

---
+++ js/backend.js
@@ -0,0 +1,173 @@
+var bawmrpFindPosts;
+
+(function( $ ){
+
+	bawmrpFindPosts = {
+		open: function( af_name, af_val ) {
+			var overlay = $( '.ui-find-overlay' );
+
+			if ( overlay.length === 0 ) {
+				$( 'body' ).append( '<div class="ui-find-overlay"></div>' );
+				bawmrpFindPosts.overlay();
+			}
+
+			overlay.show();
+
+			if ( af_name && af_val ) {
+				$( '#affected' ).attr( 'name', af_name ).val( af_val );
+			}
+
+			$( '#find-posts' ).show();
+
+			$( '#find-posts-input' ).focus().on( 'keyup', function( e ){
+				if ( e.which == 27 ) {
+					bawmrpFindPosts.close();
+				} // close on Escape
+			});
+
+			// Pull some results up by default
+			bawmrpFindPosts.send();
+
+			return false;
+		},
+
+		close : function() {
+			$('#find-posts-response').html('');
+			$('#find-posts').hide();
+			$('.ui-find-overlay').hide();
+			// $('#find-posts').draggable('destroy').hide();
+		},
+
+		overlay: function() {
+			$( '.ui-find-overlay' ).on( 'click', function () {
+				bawmrpFindPosts.close();
+			});
+		},
+
+		send: function() {
+			var $pt = '';
+			$('input[name="find-posts-what[]"]:checked').each(function(){
+				$pt += $( this ).val() + ',';
+			});
+			var post = {
+					ps: $( '#find-posts-input' ).val(),
+					action: 'rsrc_find_posts',
+					_ajax_nonce: $('#_ajax_nonce').val(),
+					post_type: $pt
+				},
+				spinner = $( '.find-box-search .spinner' );
+			spinner.show();
+			$.ajax( ajaxurl, {
+				type: 'POST',
+				data: post,
+				dataType: 'json'
+			}).always( function() {
+				spinner.hide();
+			}).done( function( x ) {
+				if ( ! x ) {
+					$( '#find-posts-response' ).text( x.responseText );
+				}
+				$( '#find-posts-response' ).html( x.data );
+			}).fail( function( x ) {
+				$( '#find-posts-response' ).text( x.responseText );
+			});
+		}
+	};
+
+	$( document ).ready(function() {
+		
+		function bawmrp_open_find_posts_dialog( e ) {
+			e.preventDefault();
+			bawmrpFindPosts.open( 'from_post', bawmrp_js.ID ); 
+		}
+
+		$( '#find-posts-submit' ).on( 'click', function( e ) {
+			if ( '' == $( '#find-posts-response' ).html() ) {
+				e.preventDefault();
+			}
+		});
+		$( '#find-posts .find-box-search :input' ).on( 'keypress', function( e ) {
+			if ( 13 == e.which ) {
+				bawmrpFindPosts.send();
+				return false;
+			}
+		} );
+		$( '#find-posts-search' ).on( 'click', bawmrpFindPosts.send );
+		$( '#find-posts-close' ).on( 'click', bawmrpFindPosts.close );
+			
+		$( '#bawmrp_open_find_posts_button' ).on( 'click', bawmrp_open_find_posts_dialog );
+		
+		$( '#bawmrp_delete_related_posts' ).on( 'click', function() {
+			$( '.related-posts' ).animate( { opacity: 0 }, 500, function() { 
+																$( this ).html( '' ) ;
+																$( '#bawmrp_post_ids' ) .val( '' );
+																$( this ).css( 'opacity', '1' ) ;
+															}
+			);
+		} );
+				
+		$( 'body:first' ).prepend( $( '.find-box-search input#_ajax_nonce' ) );
+		
+		$( ".related-posts" ).sortable({
+			'update' : function( e, ui ) {
+				var ids = [];
+				$( '.related-posts li' ).each( function( i, item ) {
+					ids.push( $( item ).attr( 'data-id' ) );
+				});
+				$( '#bawmrp_post_ids' ).val( ids.join( ',' ) );
+			},
+			'revert': true,
+			'placeholder': 'sortable-placeholder',
+			'tolerance': 'pointer',
+			'axis': 'y',
+			'containment': 'parent',
+			'cursor': 'move',
+			'forcePlaceholderSize': true,
+			'dropOnEmpty': false,
+		});
+		
+		$( '#find-posts-submit' ).on( 'click', function( e ) {
+			e.preventDefault();
+			if ( $( 'input[name="found_post_id[]"]:checked' ).length == 0 ) {
+				return false;
+			}
+			$( 'input[name="found_post_id[]"]:checked' ).each( function( id ) {
+				var selectedID = $( this ).val();
+				var posts_ids = new Array();
+				posts_ids = $( '#bawmrp_post_ids' ).val() != '' ? $( '#bawmrp_post_ids' ).val().split( ',' ) : [];
+				if ( $.inArray( selectedID, posts_ids ) == '-1' && selectedID != bawmrp_js.ID ) {
+					posts_ids.push( selectedID );
+					$( '#bawmrp_post_ids' ).val( posts_ids );
+					$( this ).parent().parent().css( 'background', '#ff0000' ).fadeOut( 500, function() { $( this ).remove() } );
+					var label = $( this ).parent().next().text();
+					label = label.replace( /</g, '&lt;' ).replace( />/g, '&gt;' );
+					var elem_li = '<li data-id="' + selectedID + '"><span><a class="erase_yyarpp"><span class="dashicons dashicons-dismiss"></span></a>&nbsp;&nbsp;' + label + '</span></li>';
+					$( '.related-posts' ).append( elem_li );
+				}
+			});
+			return false;			
+		});
+
+		setInterval( function()	{
+			if ( $( '#find-posts-response input:checkbox' ).length > 0 ) {
+				var $forbidden_ids = $( '#bawmrp_post_ids' ).val().split( ',' );
+				$( '#find-posts-response input[value="' + bawmrp_js.ID + '"]' )
+					.attr( 'disabled', 'disabled' );
+				$( '#find-posts-response input' ).filter( function( i ) { 
+						return $.inArray( $( this ).val(), $forbidden_ids ) >- 1;
+				} )
+				.attr( 'disabled', 'disabled' ).attr( 'checked', 'checked' );
+			}
+		}, 100 );
+		
+		$( '.erase_yyarpp' ).on( 'click', function() {
+			var id = $( this ).parent().parent().attr( 'data-id' );
+			$( this ).parent().parent().fadeOut( 500, function() { $( this ).remove() } );
+			var posts_ids = ',' + $( '#bawmrp_post_ids' ).val() + ',';
+			posts_ids = posts_ids.replace( ',' + id + ',', ',' );
+			$( '#bawmrp_post_ids' ).val( posts_ids.length > 1 ? posts_ids.substring( 1, posts_ids.length-1 ) : '' );
+		});
+		
+	});
+	
+})(jQuery);			
---
+++ really-simple-related-content.php
@@ -0,0 +1,168 @@
+<?php
+/*
+Plugin Name: Really Simple Related Content
+Plugin URI: http://wordpress.org/plugins/really-simple-related-content/
+Version: v1.0
+Plugin URI:
+Author: Oscar Ciutat
+Author URI: http://oscarciutat.com/code/
+Description: A really simple manual related posts plugin
+*/
+
+class Really_Simple_Related_Content {
+
+	/**
+	 * Plugin instance.
+	 *
+	 * @since 1.0
+	 *
+	 */
+	protected static $instance = null;
+
+
+	/**
+	 * Access this plugin’s working instance
+	 *
+	 * @since 1.0
+	 *
+	 */
+	public static function get_instance() {
+		
+		if ( !self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+
+	}
+
+	
+	/**
+	 * Used for regular plugin work.
+	 *
+	 * @since 1.0
+	 *
+	 */
+	public function plugin_setup() {
+
+  		$this->includes();
+
+        add_action( 'init', array( $this, 'load_language' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'get_related_posts', array( $this, 'get_related_posts' ) );
+	}
+
+	
+	/**
+	 * Constructor. Intentionally left empty and public.
+	 *
+	 * @since 1.0
+	 *
+	 */
+	public function __construct() {}
+	
+	
+ 	/**
+	 * Includes required core files used in admin and on the frontend.
+	 *
+	 * @since 1.0
+	 *
+	 */
+	protected function includes() {
+		include_once 'includes/class-admin.php';
+	}
+
+
+	/**
+	 * Loads language
+	 *
+	 * @since 1.0
+	 *
+	 */
+	function load_language() {
+		load_plugin_textdomain( 'really-simple-related-content', '', dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	
+	/**
+	 * enqueue_scripts
+	 */
+	function enqueue_scripts() {
+		wp_enqueue_style( 'really-simple-manual-related-posts-css', plugins_url( '/style.css', __FILE__) );
+	}
+
+
+	/*
+	* get_related_posts_ids
+	*/
+
+	function get_related_posts_ids( $post_id ) {
+		$settings = (array) get_option( 'rsrc_settings' );
+		$ids = get_post_meta( $post_id, '_rsrc_posts_ids', true );
+		$ids = ! empty( $ids ) ? implode( ',', wp_parse_id_list( $ids ) ) : array();
+		return $ids;
+	}
+
+	
+	/*
+	* get_related_posts
+	*/
+
+	function get_related_posts( $post_id ) {
+		$settings = (array) get_option( 'rsrc_settings' );
+		$header = isset( $settings['header'] ) ? $settings['header'] : __( 'Related Posts', 'really-simple-related-content' );
+		$layout = isset( $settings['layout'] ) ? $settings['layout'] : 'thumbnails';
+		$thumbnail_size = isset( $settings['thumbnail_size'] ) ? $settings['thumbnail_size'] : 'thumbnail';
+		echo $thumbnail_size;
+		$ids = wp_parse_id_list( $this->get_related_posts_ids( $post_id ) );
+		if ( ! empty( $ids ) ) {
+			$html = '<div class="related-posts"><h3>' . $header . '</h3><ul class="layout-' . $layout . '">';
+			foreach( $ids as $id ) {
+				if ( $layout == 'list' ) {
+					$html .= '<li><a href="' . esc_url( apply_filters( 'the_permalink', get_permalink( $id ) ) ) . '">' . get_the_title( $id ) . '</a></li>';	
+				} else {
+					$html .= '<li>' . $this->get_post_thumbnail( $id, $thumbnail_size ) . '</li>';
+				}
+			}
+			$html .= '</ul></div>';
+			echo $html;
+		}
+
+	}
+
+	
+	/*
+	* get_post_thumbnail
+	*/
+
+	function get_post_thumbnail( $id = null, $size = 'thumbnail' ) {
+		$post = get_post( $id );
+		
+		$html = '<a href="' . get_permalink( $id ) . '"><div class="thumb">';
+		if ( has_post_thumbnail( $id ) ) {
+			$image_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), $size );
+			$html .= '<img src="' . $image_attributes[0] . '" width="' . $image_attributes[1] . '" height="' . $image_attributes[2] . '" alt="' . esc_attr( get_the_title( $id ) ) . '" />';
+		} else {
+			if ( isset( $_wp_additional_image_sizes ) && count( $_wp_additional_image_sizes ) && in_array( $size, array_keys( $_wp_additional_image_sizes ) ) ) {
+				$width = $_wp_additional_image_sizes[$size]['width'];
+				$height = $_wp_additional_image_sizes[$size]['height'];
+			} else {
+				$width = get_option( $size. '_size_w' );
+				$height = get_option( $size. '_size_h' );
+			} 
+			$placeholder = apply_filters( 'really_simple_related_content_placeholder_url', plugins_url( '/assets/images/placeholder.png', __FILE__ ) );
+			$html .= '<img src="' . $placeholder . '" width="' . $width . '" height="' . $height . '" alt="' . esc_attr( get_the_title( $id ) ) . '" />';
+		}
+		
+		$html .= '<div class="overlay">
+				<div class="entry-caption">
+				<div class="entry-title">' . get_the_title( $id ) . '</div>';
+				
+		$html .= '</div></div></div></a>';
+
+		return $html;
+	}
+
+}
+
+add_action( 'plugins_loaded', array ( Really_Simple_Related_Content::get_instance(), 'plugin_setup' ) );

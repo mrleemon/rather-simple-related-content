86c8bf3b06febaa7dc1ca919950fdcbbf9d5dac7
leemon <tacrapo@gmail.com>
2024-04-14 19:28:03 +0200

    Update Requires PHP to 7.4

--- rather-simple-related-content.php
+++ rather-simple-related-content.php
@@ -5,7 +5,7 @@
  * Update URI: false
  * Version: 1.0
  * Requires at least: 5.0
- * Requires PHP: 7.0
+ * Requires PHP: 7.4
  * Author: Oscar Ciutat
  * Author URI: http://oscarciutat.com/code/
  * Text Domain: rather-simple-related-content
